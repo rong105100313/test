fe27e27a9ea1990ea6951cf4e8c3c01f1dbdbfde
rong105100313 <80232821+rong105100313@users.noreply.github.com>
2021-03-12 17:12:06 +0800

    Add files via upload

---
+++ 第19章_隨堂練習四_碰牆的球體運動_自己想的.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 27 16:15:45 2020
+
+@author: GameToGo
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 27 15:40:40 2020
+
+@author: GameToGo
+"""
+
+import turtle 
+turtle.setup(width=600,height=400,startx=200,starty=200)
+
+def ball():
+    turtle.hideturtle()
+    #turtle.speed(0)
+    turtle.color("red","pink")  
+    turtle.pensize(2)
+    turtle.penup()
+    turtle.goto(-270,0)
+    for i in range(56):      
+        #底下開始隱藏作畫過程
+        turtle.tracer(False)
+        turtle.clear() 
+        turtle.pendown() 
+        turtle.begin_fill()
+        turtle.circle(20,360,200)  
+        turtle.end_fill() 
+        turtle.update()   #更新畫面
+    #底下開始顯示以上的繪圖結果，n為更新畫面次數，delay為延遲ms。
+        turtle.tracer(n=1,delay=50) 
+        turtle.penup()     
+        turtle.forward(9.8)
+        
+    b=str(1)
+    return b
+
+while(True):
+    a=ball()
+    print('碰到牆了'+a[0])
+    if a[0]=='1':
+        turtle.hideturtle()
+        #turtle.speed(0)
+        turtle.color("red","pink")  
+        turtle.pensize(2)
+        turtle.penup()
+        turtle.goto(270,0)
+        for i in range(56):
+            #底下開始隱藏作畫過程
+            turtle.tracer(False)
+            turtle.clear() 
+            turtle.pendown() 
+            turtle.begin_fill()
+            turtle.circle(20,360,200)  
+            turtle.end_fill() 
+            turtle.update()   #更新畫面
+        #底下開始顯示以上的繪圖結果，n為更新畫面次數，delay為延遲ms。
+            turtle.tracer(n=1,delay=50) 
+            turtle.penup()     
+            turtle.backward(9.9)
+        
+turtle.mainloop()
