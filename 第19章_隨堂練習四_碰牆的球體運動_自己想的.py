bed498dd5ca39f187475d9fb22afd274b2c54b58
rong105100313 <rong105100313@gmail.com>
2021-03-14 22:15:39 +0800

    nww

--- 第19章_隨堂練習四_碰牆的球體運動_自己想的.py
+++ 第19章_隨堂練習四_碰牆的球體運動_自己想的.py
@@ -1,15 +1,24 @@
-#第一版 原始
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 27 15:09:41 2020
+
+@author: GameToGo
+"""
+
 import turtle 
 turtle.setup(width=600,height=400,startx=200,starty=200)
 
-def ball():
+
+def turtle2():
     turtle.hideturtle()
     #turtle.speed(0)
     turtle.color("red","pink")  
     turtle.pensize(2)
     turtle.penup()
-    turtle.goto(-270,0)
-    for i in range(56):      
+    turtle.goto(-280,0)
+    x=2
+    right=True
+    while(True):      
         #底下開始隱藏作畫過程
         turtle.tracer(False)
         turtle.clear() 
@@ -19,35 +28,17 @@ def ball():
         turtle.end_fill() 
         turtle.update()   #更新畫面
     #底下開始顯示以上的繪圖結果，n為更新畫面次數，delay為延遲ms。
-        turtle.tracer(n=1,delay=50) 
+        turtle.tracer(n=1,delay=2) 
         turtle.penup()     
-        turtle.forward(9.8)
+        turtle.forward(x)
         
-    b=str(1)
-    return b
-
-while(True):
-    a=ball()
-    print('碰到牆了'+a[0])
-    if a[0]=='1':
-        turtle.hideturtle()
-        #turtle.speed(0)
-        turtle.color("red","pink")  
-        turtle.pensize(2)
-        turtle.penup()
-        turtle.goto(270,0)
-        for i in range(56):
-            #底下開始隱藏作畫過程
-            turtle.tracer(False)
-            turtle.clear() 
-            turtle.pendown() 
-            turtle.begin_fill()
-            turtle.circle(20,360,200)  
-            turtle.end_fill() 
-            turtle.update()   #更新畫面
-        #底下開始顯示以上的繪圖結果，n為更新畫面次數，delay為延遲ms。
-            turtle.tracer(n=1,delay=50) 
-            turtle.penup()     
-            turtle.backward(9.9)
-        
+        if turtle.xcor() >= 280 and right==True:
+            x=-2
+            right=False
+        if turtle.xcor() <= -280 and right==False:
+            x=2
+            right=True
+    
+turtle.setup(width=600,height=400,startx=200,starty=200)
+turtle2()
 turtle.mainloop()
